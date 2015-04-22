58cc694a5a40f5988c94f25fb7b2e065d80ce9cc
Michael Große <mic.grosse+github@googlemail.com>
2015-04-22 14:20:10 +0200

    Initial commit

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the lawlink plugin
+ *
+ * @author Michael Große <mic.grosse@posteo.de>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the lawlink plugin
+ *
+ * @author Michael Große <mic.grosse@posteo.de>
+ */
+
+
+//$meta['fixme'] = array('string');
+
